08ff2c5b98ce5fb785d2ea73d7732300473e9a1e
Aaron Melendez <aaronmelendez37@gmail.com>
2025-10-26 23:02:47 -0700

    wip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /** @jsx createElement */
 function createElement(type, props, children) {
   if (props === null) props = {};
+  console.log("createElement: ", { type, props, children });
   return { type, props, children };
 }
 
@@ -15,22 +16,32 @@ function setAttribute(dom, key, value) {
     dom[key] = value;
   } else if (key === "style" && typeof value === "object") {
     Object.assign(dom.style, value);
+  } else if (key === "ref") {
+    key.current = dom;
+  } else if (key === "key") {
+    dom.__key = value;
+  } else if (typeof value != "object" && typeof value != "function") {
+    dom.setAttribute(key, value);
   }
 }
 
 function render(vdom, parent) {
+  console.log("vdom: ", vdom);
   const mount = (el) => parent.appendChild(el);
   if (typeof vdom === "string" || typeof vdom === "number") {
-    return mount(document.createTextNode(vdom.children));
+    // mount strings and numbers to dom as string
+    return mount(document.createTextNode(vdom));
   } else if (typeof vdom === "boolean" || vdom === null) {
+    // mount booleans as empty string
     return mount(document.createTextNode(""));
   } else if (typeof vdom === "object" && typeof vdom.type === "function") {
     // jsx parser passes custom component object itself as vdom argument
     // Component handles the rendering in this case
-    Component.render(vdom, parent);
+    return Component.render(vdom, parent);
   } else if (typeof vdom === "object" && typeof vdom.type === "string") {
+    // native jsx components mounted to dom as corresponding html elements
     const dom = mount(document.createElement(vdom.type), parent);
-    for (const child of [...vdom.children]) render(child, dom);
+    for (const child of [].concat(vdom.children)) render(child, dom);
     for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
     return dom;
   } else {
@@ -38,7 +49,48 @@ function render(vdom, parent) {
   }
 }
 
-function patch() {}
+function patch(dom, vdom, parent = dom.parentNode) {
+  const replace = (el) => parent.replaceChild(el, dom) && el;
+  if (typeof vdom === "object" && vdom.type === "function") {
+    // call Component patch for custom components
+    return Component.patch(dom, vdom, parent);
+  } else if (typeof vdom != "object" && dom instanceof Text) {
+    // unmatching text content
+    return dom.textContent != vdom ? replace(render(vdom, parent)) : dom;
+  } else if (typeof vdom == "object" && dom instanceof Text) {
+    // replace text with vdom
+    return replace(render(vdom, parent));
+  } else if (
+    typeof vdom == "object" &&
+    dom.nodeName != vdom.type.toUpperCase()
+  ) {
+    // replace unmatching dom node with vdom
+    return replace(render(vdom, parent));
+  } else if (typeof vdom == "object" && dom.nodeName == vdom.type.upperCase()) {
+    // perform patch when vdom type matches current dom node
+    const pool = {};
+    const activeElement = document.activeElement;
+    [...dom.childNodes].map((child, index) => {
+      const key = child.__key || `index_${index}`;
+      pool[key] = child;
+    });
+    [...vdom.children].map((child, index) => {
+      const key = child.props.__key || `index_${index}`;
+      pool[key] ? patch(pool[key], child) : render(child, parent);
+      delete pool[key];
+    });
+    for (const key in pool) {
+      const instance = pool[key].__instance;
+      if (instance) instance.componentWillUnmount();
+      pool[key].remove();
+    }
+    // remove all preexisting dom attributes and add potentially updated ones from vdom
+    for (const attr of dom.attributes) dom.removeAttribute(attr.name);
+    for (const prop of vdom.props) setAttribute(dom, prop, vdom.props[prop]);
+    activeElement.focus();
+    return dom;
+  }
+}
 
 class Component {
   constructor(props = {}) {
@@ -46,11 +98,11 @@ class Component {
     this.state = null;
   }
 
-  render(vdom, parent) {
+  static render(vdom, parent) {
     const props = Object.assign({}, vdom.props);
     // class components
     if (Component.isPrototypeOf(vdom.type)) {
-      const instance = new vdom(props);
+      const instance = new vdom.type(props);
       instance.componentWillMount();
       instance.base = render(instance.render(), parent);
       instance.__instance = instance;
@@ -63,11 +115,73 @@ class Component {
     }
   }
 
-  setState() {}
+  static patch(dom, vdom, parent = dom.parentNode) {
+    // if dom is from vdom constructor
+    if (dom.__instance && dom.__instance.constructor === vdom.type) {
+      // assign props and patch
+      const nextProps = Object.assign({}, vdom.props, vdom.children);
+      this.willReceiveProps(props);
+      this.props = nextProps;
+      return patch(dom, vdom, parent);
+    } else if (Component.isPrototypeOf(vdom)) {
+      const newDom = Component.render(vdom, parent);
+      return parent ? parent.replaceChild(newDom, dom) && dom : newDom;
+    } else if (!Component.isPrototypeOf(vdom)) {
+      return patch(dom, vdom.type(props), parent);
+    }
+  }
 
-  componentWillMount() {}
+  setState(next) {
+    const compatible = (nextState) =>
+      typeof this.state == "object" && typeof nextState == "object";
+    if (this.base && this.componentShouldUpdate()) {
+      this.componentWillUpdate();
+      this.state = compatible(next)
+        ? Object.assign({}, this.state, next)
+        : next;
+      patch(this.base, this.render());
+      this.componentDidUpdate();
+    }
+  }
 
-  componentDidMount() {}
+  componentShouldUpdate() {
+    return true;
+  }
+
+  componentWillMount() {
+    return undefined;
+  }
+
+  componentDidMount() {
+    return undefined;
+  }
+
+  componentWillUnmount() {
+    return undefined;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    return undefined;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    return undefined;
+  }
+
+  componentDidUpdate(nextProps, nextState) {
+    return undefined;
+  }
 }
 
-render(<p>{true}</p>, document.getElementById("root"));
+class SimpleComponent extends Component() {
+  render() {
+    return (
+      <div>
+        <p>hello world 1</p>
+        <p>hello world 2</p>
+      </div>
+    );
+  }
+}
+
+render(<SimpleComponent />, document.getElementById("root"));
